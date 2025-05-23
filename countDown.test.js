531ab24e635bc7cbde428d2c53a43d4e62f828fb
Papa1881 <papaleon18@gmail.com>
2025-05-23 09:24:27 +0500

    Update countDown.test.js

--- countDown.test.js
+++ countDown.test.js
@@ -1,1 +1,24 @@
+const countDown = require('./countDown');
 
+describe('countDown', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    console.log.mockRestore();
+  });
+
+  test('prints 3 2 1 when input is 3', () => {
+    countDown(3);
+    expect(console.log.mock.calls).toEqual([[3], [2], [1]]);
+  });
+
+  test('throws error when input is 0', () => {
+    expect(() => countDown(0)).toThrow("Invalid input");
+  });
+
+  test('throws error when input is a string', () => {
+    expect(() => countDown("abc")).toThrow("Invalid input");
+  });
+});
